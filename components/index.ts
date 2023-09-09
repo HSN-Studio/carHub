1febc18de9937ef67d6a818d36d16653bd550755
HSN-Studio <hassanrafique198@gmail.com>
2023-09-10 02:34:25 +0500

    Revert "redo commit"
    This reverts commit fd19e4b80d2c9d1fbe9eefc2c950e7ae28f18a85.

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,20 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-export { Hero, CustomButton, Navbar, Footer };
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+};
