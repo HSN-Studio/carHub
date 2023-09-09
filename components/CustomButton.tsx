fd19e4b80d2c9d1fbe9eefc2c950e7ae28f18a85
HSN-Studio <hassanrafique198@gmail.com>
2023-09-10 02:32:13 +0500

    redo commit

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,8 +7,6 @@ const CustomButton = ({
   containerStyles,
   handleClick,
   btnType,
-  textStyles,
-  rightIcon,
 }: customButtonProps) => {
   return (
     <button
@@ -17,17 +15,7 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1 ${textStyles}`}>{title}</span>
-      {rightIcon && (
-        <div className="relative w-6 h-6">
-          <Image
-            src={rightIcon}
-            alt="right icon"
-            className="object-contain"
-            fill
-          />
-        </div>
-      )}
+      <span className={`flex-1`}>{title}</span>
     </button>
   );
 };
